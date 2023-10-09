ee5e821a6e6b5a2b1c34be341b91fae4dc8e9c64
Satyam <satyammitthu@gmail.com>
2023-10-09 08:23:36 +0530

    Sumof2no

---
+++ Day 1/Sumof2no.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a, b, sum ;
+	cout<<"type a number ";
+	cin>>a;
+	cout<<"type another number ";
+	cin>>b;
+	sum=a+b;
+	cout<<"sum is : "<<sum<<endl;
+	
+}
